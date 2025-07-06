3e080e149b7b550f29b5af2cec8635ffa4e93fd7
Ahmed Rizk <rezk18649@gmail.com>
2025-07-06 18:38:47 +0300

    add custom custom list view item to home view

--- lib/features/home/presentation/views/widgets/custom_app_bar.dart
+++ lib/features/home/presentation/views/widgets/custom_app_bar.dart
@@ -8,7 +8,7 @@ class CustomAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 48.0, horizontal: 16),
+      padding: const EdgeInsets.only(top: 48.0,bottom: 32, left: 16,right: 16),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
