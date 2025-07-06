071595793a2fa24d3ece751eacc51114d153d593
Ahmed Rizk <rezk18649@gmail.com>
2025-07-06 15:55:22 +0300

    Add  sliding animation to splash view

--- lib/features/presentation/views/widgets/splash_view_body.dart
+++ lib/features/presentation/views/widgets/splash_view_body.dart
@@ -1,8 +1,39 @@
-
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    animationController = AnimationController(
+      vsync: this,
+      duration: Duration( seconds: 2),
+    );
+
+    slidingAnimation = Tween(
+      begin: Offset(0, 2),
+      end: Offset(0, 0),
+    ).animate(animationController);
+
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,16 +42,22 @@ class SplashViewBody extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Image.asset("assets/images/Logo.png"),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("The books you need are here",style: TextStyle(
-              color: Colors.white,
-              fontSize: 18
-            ),),
-          ],
-        )
+        AnimatedBuilder(
+          animation: slidingAnimation,
+          builder: (BuildContext context, Widget? child) {
+            return SlideTransition(
+              position: slidingAnimation,
+              child: Text(
+                "Read Free Books",
+                style: TextStyle(),
+                textAlign: TextAlign.center,
+              ),
+            );
+          },
+        ),
       ],
     );
   }
+
+
 }
